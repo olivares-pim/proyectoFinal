39b36dfcf7ea746e4fa96a2288982a8137bfcd34
Damilk-TB <20071563@ce.pucmm.edu.do>
2025-07-23 20:55:11 -0400

    Creacion clase

--- componentesCuezt/src/logico/Factura.java
+++ componentesCuezt/src/logico/Factura.java
@@ -2,13 +2,43 @@ package logico;
 
 public class Factura {
 	
-	private String id;
-
-	public String getId() {
-		// Igual que el getter que hice en cliente, un getter temporal
-		return this.id;
+	private int id_factura;
+	private int id_cliente;
+	private int id_combo;
+	private int fechaPedido;
+	private float precioTotal;
+	public int getId_factura() {
+		return id_factura;
+	}
+	public void setId_factura(int id_factura) {
+		this.id_factura = id_factura;
+	}
+	public int getId_cliente() {
+		return id_cliente;
+	}
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+	public int getId_combo() {
+		return id_combo;
+	}
+	public void setId_combo(int id_combo) {
+		this.id_combo = id_combo;
+	}
+	public int getFechaPedido() {
+		return fechaPedido;
+	}
+	public void setFechaPedido(int fechaPedido) {
+		this.fechaPedido = fechaPedido;
+	}
+	public float getPrecioTotal() {
+		return precioTotal;
+	}
+	public void setPrecioTotal(float precioTotal) {
+		this.precioTotal = precioTotal;
 	}
 
+	
 }
 
 
