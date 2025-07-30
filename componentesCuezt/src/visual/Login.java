09d2b12cd216bb5bc5857f0dc153a8c41ead4bc6
olivares-pim <20150431@ce.pucmm.edu.do>
2025-07-29 22:28:52 -0400

    Cambios a login: 	Ya no es resizable

--- componentesCuezt/src/visual/Login.java
+++ componentesCuezt/src/visual/Login.java
@@ -9,6 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import logico.Tienda;
+import logico.Usuario;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -20,6 +21,8 @@ import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Login extends JDialog {
 
@@ -47,8 +50,9 @@ public class Login extends JDialog {
 	public Login() {
 		setTitle("Log In");
 		setBounds(100, 100, 600, 400);
+		setResizable(false);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPanel.setBorder(new EmptyBorder(20, 120, 20, 120));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		setLocationRelativeTo(null);
 		contentPanel.setLayout(new GridLayout(0, 1, 0, 0));
@@ -58,6 +62,12 @@ public class Login extends JDialog {
 		contentPanel.add(lblLogin);
 		
 		txtUser = new JTextField();
+		txtUser.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				lblWarning.setVisible(false);
+			}
+		});
 		txtUser.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
@@ -75,6 +85,12 @@ public class Login extends JDialog {
 		contentPanel.add(lblPassword);
 		
 		pwdPassword = new JPasswordField();
+		pwdPassword.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				lblWarning.setVisible(false);
+			}
+		});
 		pwdPassword.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
@@ -107,9 +123,15 @@ public class Login extends JDialog {
 	
 	public void login() {
 		if(Tienda.getInstance().login(txtUser.getText(), pwdPassword.getText())) {
+			String userType = Tienda.getInstance().getUserByUsername(txtUser.getText()).getTipo();
+			if(userType.equals("Registrador")) {
+				RegUsuario regFrame = new RegUsuario();
+				regFrame.setVisible(true);				
+			} else {
 			Principal frame = new Principal();
-			dispose();
 			frame.setVisible(true);
+			}
+			dispose();
 		}else {
 			lblWarning.setVisible(true);
 			clean();
